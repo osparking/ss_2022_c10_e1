753f168d6d78871b88b60a87783038956130265b
osparking <jbpark03@gmail.com>
2023-08-25 11:03:11 +0900

    project - spring security spring boot 3

---
+++ src/test/java/space/bum/c10_e1/Ss2022C10E1ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.c10_e1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2022C10E1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
