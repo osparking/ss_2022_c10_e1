9030b30d738785e9fae9140bea3f6d578583f7f1
osparking <jbpark03@gmail.com>
2023-08-25 14:45:31 +0900

    more programmatic way of request security config

--- src/main/java/space/bum/c10_e1/config/SecurityConfig.java
+++ src/main/java/space/bum/c10_e1/config/SecurityConfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 public class SecurityConfig {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests()
-				.requestMatchers("/demo").authenticated();
+				.requestMatchers("/demo")
+					.access(new WebExpressionAuthorizationManager(
+											"isAuthenticated()"));
 		
 		return http.build();
 	}
